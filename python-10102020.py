10615cacb4dcd46e5612018f2b8b46d08bd7c88f
Sudhir Tomar <sudhir@skive.it>
2020-10-10 19:56:59 -0700

    commit

---
+++ python-10102020.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("hello world in much fun")
+x=10
+print(x)
+print(type(x))
+s = "python is fun"
+print(type(s))
+print("value of x is:",x)
+# print("value of y is:",y)
+cars =100
+drivers = 80
+print("there are cars ", cars, "and drivers ", drivers)
+print ("car pool cpacity is ", drivers*4)
+print("theer sre {} cars and {} drivers".format(cars,drivers))
+print(f"theer sre {cars} cars and {drivers} drivers")
+# built-in functions -- print, type
