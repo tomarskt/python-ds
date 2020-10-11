73f3409ed3276691c64cd42081461c219698d98b
Sudhir Tomar <sudhir@skive.it>
2020-10-10 20:03:12 -0700

    commit

--- python-10102020.py
+++ python-10102020.py
@@ -13,4 +13,15 @@ print("there are cars ", cars, "and drivers ", drivers)
 print ("car pool cpacity is ", drivers*4)
 print("theer sre {} cars and {} drivers".format(cars,drivers))
 print(f"theer sre {cars} cars and {drivers} drivers")
+print("theer sre %d cars and %d drivers"%(cars,drivers))
+x=100**123
+print(x)
+x+7635
+print(x)
+x=10
+y=20
+print(x+7-y)
+print(x*6)
+y=6.8
+print(type(y))
 # built-in functions -- print, type
