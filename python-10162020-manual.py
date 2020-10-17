3a191fd9d42a2d57f50f1e2f40cae9fafe912929
Sudhir Tomar <sudhir@skive.it>
2020-10-16 20:50:33 -0700

    print("*****************")

--- python-10162020-manual.py
+++ python-10162020-manual.py
@@ -51,3 +51,46 @@ print(len(d))
 print(d['a'])
 d['a'] += 'two'
 print(d)
+print("*****************")
+for i in mydict:
+    print(i)
+    print(mydict[i])
+print("*****************")
+for i in mydict.values():
+    print(i)
+    # print(mydict[i])
+print("*****************")
+for i in mydict.keys():
+    print(i)
+    # print(mydict[i])
+
+print(mydict.keys())
+print(mydict.values())
+print(mydict.items())
+
+print("*****************")
+for i in mydict.items():
+    print(i)
+
+print("*****************")
+for i in mydict.items():
+    print(i[0],'->',i[1])
+print("*****************")
+ladies_purse = {'cards':['Amex','IcICI'], 'cash': 1800, 'candies': 3}
+print(ladies_purse)
+for key in ladies_purse.keys():
+    # print(key)
+    # print(ladies_purse[key])
+    if(key == 'cards'):
+        # print(ladies_purse[key])
+        for val in ladies_purse[key]:
+            # print(val)
+            if val == 'IcICI':
+                print(val[1])
+print("*****************")
+lad = {'cards':['Amex','IciCI'],'cash':1800}
+s = lad['cards']
+print(s)
+print(s[1][1])
+print("*****************")
+print(ladies_purse.get('cards')[1][1])
