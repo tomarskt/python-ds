ba49154ba9efbdb2a301c897d347bcf3d313156e
Sudhir Tomar <sudhir@skive.it>
2020-10-16 19:50:28 -0700

    if

--- python-10162020-manual.py
+++ python-10162020-manual.py
@@ -1,1 +1,11 @@
 print("hello world")
+lst=[2,4,5]
+# print(lst*2)
+lst.extend('python')
+lst.append('NEW')
+print(lst)
+files=['first.py','second','third.py','4th','fifth','1.py']
+print(files)
+for file in files:
+    if file.endswith('py'):
+        print(file)
